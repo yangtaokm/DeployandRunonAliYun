e84ea273c39bed24a98017bb3440f2a5d0a0832a
yangtaokm <yangtao_work@hotmail.com>
2016-07-30 09:50:02 -0700

    Upload source code to github

---
+++ AliSF/CogAPI/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ProjectOxford.Face;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using System;
+using System.Diagnostics;
+
+namespace CogAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+  
+        // POST api/values 
+        public async Task<string> Get(string imgUrl)
+        {
+            
+            VisionServiceClient visionclient = new VisionServiceClient("CognitiveServiceVisionKey");
+            AnalysisResult result = null;
+            try
+            {
+               result = await visionclient.DescribeAsync(imgUrl);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+           return result.Description.Captions[0].Text;
+        }
+
+    }
+}
